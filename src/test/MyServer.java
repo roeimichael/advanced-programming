0c5e34fea01b083ac3e1ea76fb1280c17ae3173d
roeimichael <roeym111@gmail.com>
2023-04-09 20:15:04 +0300

    Revert "MyServer"
    This reverts commit 2c2e6bc38e35206668c535f77d02430f11abd25a.

--- src/test/MyServer.java
+++ src/test/MyServer.java
@@ -1,53 +1,7 @@
 package test;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.SocketTimeoutException;
 
 public class MyServer {
-	int port;
-	ClientHandler ch;
-	int numOfThreads;
-	boolean stop;
-	
-	MyServer(int port,ClientHandler ch, int numOfThreads)
-	{
-		this.port=port;
-		this.numOfThreads=numOfThreads;
-		this.ch=ch;
-		
-	}
-	
-	public void start()
-	{
-		stop=false;
-		new Thread(()->startsServer()).start();
-
-	}
-	
-	private void startsServer() {
-		try {
-			ServerSocket server = new ServerSocket(port);
-			server.setSoTimeout(1000);
-			while(!stop) {
-				try {
-					Socket client=server.accept();
-					ch.handleClient(client.getInputStream(), client.getOutputStream());
-					ch.close();
-					client.close();
-				}catch(SocketTimeoutException e) {}
-			}
-			server.close();
-	}catch(IOException e) {
-		e.printStackTrace();
-		}
-	}
-
-	public void close()
-	{
-		stop=true;
-	}
 
 	
 }
